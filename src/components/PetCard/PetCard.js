088dc5018660b967d1d7a783bc8fa35808910073
GustavoSierra <gsierra.business@gmail.com>
2021-12-02 23:57:05 +0100

    PetCard changes and APIConsumer progress

--- src/components/PetCard/PetCard.js
+++ src/components/PetCard/PetCard.js
@@ -6,20 +6,24 @@ const PetCard = () => {
     const [name, setFullname] = useState('Nombre')
     const [animal, setEmail] = useState('email')
 
-    const navigate = useNavigate
+    const navigate = useNavigate()
+
+    const handleChanges = (e) => {
+        navigate('/userProfile')
+    }
 
 
     return (
         <div className="Card">
             <div className="upperCard">
                 <div className="imageContainer">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbuzFZ5rIL1kMLIqJkp5hi8i6vgZdSt69x7A&usqp=CAU" alt="profilePic" height="100px" width="100px"/>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJ4lfqndG6xJBwplvtml62IMS0DGox3c44Kw&usqp=CAU" alt="profilePic" height="100px" width="100px"/>
                 </div>
             </div>
             <div className="lowerCard">
                 <h3>{ animal }</h3>
                 <h4>{ name }</h4>
-                <button>Ver Humano</button>
+                <button onClick={(e)=>handleChanges(e)}>Ver Humano</button>
             </div>
         </div>
     )
